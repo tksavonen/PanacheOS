3c4e6442133c6cc8fbb1f86ca5490527fdb4f6ba
Tiitus Savonen <tiitus.savonen@tuni.fi>
2025-12-11 02:47:50 +0200

    update

--- code/headers/irq.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void irq_init(void);
-
-extern volatile uint32_t timer_ticks;
--- code/headers/ports.h
+++
@@ -1,6 +0,0 @@
-// ports.h - low-level port I/O for panacheOS
-#pragma once
-#include <stdint.h>
-
-uint8_t inb(uint16_t port);
-void outb(uint16_t port, uint8_t value);
